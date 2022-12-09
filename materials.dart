37066a003ebb3b621b65f46d6f000b9212c71cf5
Eldar2021 <eldiiaralmazbekov@gmail.com>
2022-12-09 10:31:53 +0600

    first commit

---
+++ materials.dart
@@ -0,0 +1,32 @@
+/*
+flutter_svg: 0.21.0+1
+
+  bool isFullSun = false;
+  bool isDayMood = true;
+  Duration duration = const Duration(seconds: 1);
+
+    List<Color> lightBgColors = [
+      const Color(0xFF8C2480),
+      const Color(0xFFCE587D),
+      const Color(0xFFFF9485),
+      if (isFullSun) const Color(0xFFFF9D80),
+    ];
+    var darkBgColors = const [
+      Color(0xFF0D1441),
+      Color(0xFF283584),
+      Color(0xFF376AB2),
+    ];
+
+    Curves.easeInOut,
+
+    fit: BoxFit.fitHeight,
+*/
+/*
+AnimatedContainer
+LinearGradient
+AnimatedPositioned
+Positioned
+
+Tab(text: 'Morning Login'),
+Tab(text: 'Night Login'),
+*/
